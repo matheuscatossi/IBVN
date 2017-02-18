b2487738fc2363932f463f2d1d30b1c00466070c
Sebastião Martins Pereira <thiagoedanielamartins@gmail.com>
2017-02-17 23:59:01 -0200

    Pequenos ajustes de layout -
    Refatoração de Models

--- app/src/main/java/com/project/impacta/ibvn/SplashScreenActivity.java
+++ app/src/main/java/com/project/impacta/ibvn/SplashScreenActivity.java
@@ -1,9 +1,13 @@
 package com.project.impacta.ibvn;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -13,6 +17,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Window wd = getWindow();
+/*
+        wd.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        wd.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        wd.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));*/
+
 
         new Handler().postDelayed(new Runnable() {
             /*
