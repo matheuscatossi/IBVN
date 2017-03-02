1889fdde4349031ae907ceac2d863f9a8638810e
SebastiaoMartinsPereira <thiagoedanielamartins@gmail.com>
2017-03-01 23:34:03 -0300

    Configuração e implementação de Api maps na activity de Reunião.

--- app/src/main/java/com/project/impacta/ibvn/InfoReuniaoActivity.java
+++ app/src/main/java/com/project/impacta/ibvn/InfoReuniaoActivity.java
@@ -1,17 +1,26 @@
 package com.project.impacta.ibvn;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.Locale;
+
 public class InfoReuniaoActivity extends AppCompatActivity {
+    private GoogleApiClient mGoogleApiClient;
+
+    GPSTracker gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_reuniao);
 
         Intent myIntent = getIntent();
@@ -42,6 +51,38 @@ public class InfoReuniaoActivity extends AppCompatActivity {
 //
 //        TextView tv_agendado_por = (TextView) findViewById(R.id.agendado_por);
 //        tv_agendado_por.setText(agendado_por);
+
+
+        ImageView iv_map_reuniao = (ImageView) findViewById(R.id.info_reuniao_iv_map);
+
+        iv_map_reuniao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                double latitude = 0, longitude = 0;
+
+                try {
+
+                    gps = new GPSTracker(InfoReuniaoActivity.this);
+
+                    if (gps.canGetLocation()) {
+                        latitude = gps.getLatitude();
+                        longitude = gps.getLongitude();
+                    } else {
+                        gps.showSettingsAlert();
+                    }
+
+                    String uri = String.format(Locale.ROOT, "http://maps.google.com/maps?saddr=%f,%f&daddr=%f,%f", latitude, longitude, latitude + 2, longitude + 3);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                    startActivity(intent);
+
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
     }
 
 
