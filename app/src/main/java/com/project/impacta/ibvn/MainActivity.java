edf2588984d4fc2e21954a32cfa8e1dcb1ca9f9c
Matheus Catossi <matheuscatossi@gmail.com>
2017-03-16 21:01:47 -0300

    Mudando conflitos

--- app/src/main/java/com/project/impacta/ibvn/MainActivity.java
+++ app/src/main/java/com/project/impacta/ibvn/MainActivity.java
@@ -25,6 +25,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
