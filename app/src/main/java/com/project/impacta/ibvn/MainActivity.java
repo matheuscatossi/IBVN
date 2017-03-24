1e9d6700234bda423c073e13d6d6dbc102ece5f0
Matheus Catossi <matheuscatossi@gmail.com>
2017-03-23 22:21:19 -0300

    Mudanças na referencia dos fragments

--- app/src/main/java/com/project/impacta/ibvn/MainActivity.java
+++ app/src/main/java/com/project/impacta/ibvn/MainActivity.java
@@ -202,10 +202,10 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    protected void onResume() {
-        super.onResume();
-        mViewPager.setCurrentItem((selectedTab != 1) ? selectedTab : 1);
-    }
+    //    protected void onResume() {
+    //        super.onResume();
+    //        mViewPager.setCurrentItem((selectedTab != 1) ? selectedTab : 1);
+    //    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -483,51 +483,5 @@ public class MainActivity extends AppCompatActivity
             return null;
         }
     }
-
-    public static class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
-
-        private int spanCount;
-        private int spacing;
-        private boolean includeEdge;
-
-        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
-            this.spanCount = spanCount;
-            this.spacing = spacing;
-            this.includeEdge = includeEdge;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            int position = parent.getChildAdapterPosition(view);
-            int column = position % spanCount;
-
-            if (includeEdge) {
-                outRect.left = spacing - column * spacing / spanCount;
-                outRect.right = (column + 1) * spacing / spanCount;
-
-                if (position < spanCount) {
-                    outRect.top = spacing;
-                }
-                outRect.bottom = spacing;
-            } else {
-                outRect.left = column * spacing / spanCount;
-                outRect.right = spacing - (column + 1) * spacing / spanCount;
-                if (position >= spanCount) {
-                    outRect.top = spacing;
-                }
-            }
-        }
-    }
-
-    static int dpToPx(int dp, Context context) {
-        Resources r = context.getResources();
-        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
-    }
-
-    private int dpToPx(int dp) {
-        Resources r = getResources();
-        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
-    }
-
 
 }
