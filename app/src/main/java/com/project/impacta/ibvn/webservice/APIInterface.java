34f3634cd8dc0a0c15076c1e748a7646086892d0
Matheus Catossi <matheuscatossi@gmail.com>
2017-04-24 19:59:08 -0300

    Adicionando Firebase na aplicação

--- app/src/main/java/com/project/impacta/ibvn/webservice/APIInterface.java
+++ app/src/main/java/com/project/impacta/ibvn/webservice/APIInterface.java
@@ -1,25 +1,15 @@
 package com.project.impacta.ibvn.webservice;
 
 
-import android.icu.text.IDNA;
-
-import com.google.android.gms.nearby.messages.Message;
-import com.google.common.collect.ObjectArrays;
 import com.project.impacta.ibvn.Utils.Constants;
 import com.project.impacta.ibvn.model.MembroModel;
 import com.project.impacta.ibvn.model.ReuniaoModel;
 
-import org.json.JSONArray;
-
-import java.lang.reflect.Array;
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 import retrofit2.http.Path;
-import retrofit2.http.Url;
 
 /**
  * Created by Matheus on 16/03/2017.
